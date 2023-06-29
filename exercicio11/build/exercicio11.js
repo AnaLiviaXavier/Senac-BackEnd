35e21b023d09977ba99aff9eb254e5d67b768e2d
AnaLiviaXavier <analiviaxavier333@gmail.com>
2023-06-28 21:53:09 -0300

    exercicio11

--- exercicio11/build/exercicio11.js
+++ exercicio11/build/exercicio11.js
@@ -1,3 +1,25 @@
 "use strict";
-console.log('TESTE');
+var setores;
+(function (setores) {
+    setores["MARKETING"] = "marketing";
+    setores["VENDAS"] = "vendas";
+    setores["FINANCEIRO"] = "financeiro";
+})(setores || (setores = {}));
+let arrayColaboradores = [
+    { nome: "Marcos", salario: 2500, setor: setores.MARKETING, presencial: true },
+    { nome: "Maria", salario: 1500, setor: setores.VENDAS, presencial: false },
+    { nome: "Salete", salario: 2200, setor: setores.VENDAS, presencial: true },
+    { nome: "João", salario: 2800, setor: setores.MARKETING, presencial: false },
+    { nome: "Josué", salario: 5500, setor: setores.VENDAS, presencial: true },
+    { nome: "Natalia", salario: 4700, setor: setores.VENDAS, presencial: true },
+    { nome: "Paola", salario: 3500, setor: setores.MARKETING, presencial: true }
+];
+function retornaColab(arrei) {
+    let selecaoColaboradores;
+    for (let i = 0; i < 7; i++) {
+        if (arrei[i].setor == 'marketing' && arrei[i].presencial == false)
+            return arrei[i];
+    }
+}
+console.log(retornaColab(arrayColaboradores));
 //# sourceMappingURL=exercicio11.js.map
--- exercicio11/src/exercicio11.ts
+++ exercicio11/src/exercicio11.ts
@@ -15,15 +15,6 @@ Considerando o `array`acima, crie um `ENUM` para os setores e um `type` para as 
 Em seguida, crie uma função que receba este `array`como parâmetro 
 e retorne apenas as pessoas do setor de marketing que trabalham presencialmente. */
 
-let arrayColaboradores: Array<any>=[
-	{ nome: "Marcos", salário: 2500, setor: "marketing", presencial: true },
-	{ nome: "Maria" ,salário: 1500, setor: "vendas", presencial: false},
-	{ nome: "Salete" ,salário: 2200, setor: "financeiro", presencial: true},
-	{ nome: "João" ,salário: 2800, setor: "marketing", presencial: false},
-	{ nome: "Josué" ,salário: 5500, setor: "financeiro", presencial: true},
-	{ nome: "Natalia" ,salário: 4700, setor: "vendas", presencial: true},
-	{ nome: "Paola" ,salário: 3500, setor: "marketing", presencial: true }
-]
 
 enum setores{
     MARKETING ='marketing',
@@ -33,6 +24,33 @@ enum setores{
 
 type colaboradores = {
     nome: string,
+	salario: number,
+	setor:setores,
+	presencial:boolean
     
 }
+
+
+let arrayColaboradores: Array<colaboradores>=[
+	{ nome: "Marcos", salario: 2500, setor: setores.MARKETING, presencial: true },
+	{ nome: "Maria" ,salario: 1500, setor: setores.VENDAS, presencial: false},
+	{ nome: "Salete" ,salario: 2200, setor: setores.VENDAS, presencial: true},
+	{ nome: "João" ,salario: 2800, setor: setores.MARKETING, presencial: false},
+	{ nome: "Josué" ,salario: 5500, setor: setores.VENDAS, presencial: true},
+	{ nome: "Natalia" ,salario: 4700, setor: setores.VENDAS, presencial: true},
+	{ nome: "Paola" ,salario: 3500, setor: setores.MARKETING, presencial: true }
+]
+
+function retornaColab(arrei:Array<colaboradores>):void{
+	// let selecaoColaboradores: Array<colaboradores>
+	// for(let i=0;i<7;i++){
+	// 	if(arrei[i].setor == 'marketing' && arrei[i].presencial == true)
+	// 	selecaoColaboradores = selecaoColaboradores.push(arrei[i])
+	// }
+	// console.log(selecaoColaboradores)
+
+	
+}
+
+retornaColab(arrayColaboradores)
 
